3d9b35d5e0cb3d14ad2c5ee34016b0e6b3c19340
Gabon <1431629699@qq.com>
2021-06-25 10:00:55 +0800

    完善getProblem功能

--- src/main/java/com/huang/springbootdemo/mapper/Pro_CompletionMapper.java
+++ src/main/java/com/huang/springbootdemo/mapper/Pro_CompletionMapper.java
@@ -1,4 +1,13 @@
 package com.huang.springbootdemo.mapper;
 
+import com.huang.springbootdemo.entity.Pro_Completion;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
 public interface Pro_CompletionMapper {
+    @Select("select * from pro_completion where pro_no=#{pro_no}")
+    Pro_Completion getCompletionProById(int pro_no);
 }
